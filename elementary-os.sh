7a3bbe2cf6947564154fb7d820671536ff40016f
Adolfo Reyes <34036509+58851@users.noreply.github.com>
2020-03-26 15:40:14 -0400

    Create elementary-os.sh

---
+++ elementary-os.sh
@@ -0,0 +1,18 @@
+
+# REMOVE APPLICATIONS
+
+# Install elementary Tweaks
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:pilip.scott/elementary0tweaks
+sudo apt update -y
+sudo apt install elementary-tweaks
+
+#
+sudo apt install firefox
+
+sudo apt install chromium-browser
+
+sudo apt install neofetch
+
+sudo apt install youtube-dl
+
