cc0057bad1175ad1091486fb98d0f886bc7a4dff
Adolfo Reyes <34036509+58851@users.noreply.github.com>
2020-03-26 16:06:11 -0400

    Update elementary-os.sh

--- elementary-os.sh
+++ elementary-os.sh
@@ -1,18 +1,29 @@
+#!/usr/bin/env bash
 
-# REMOVE APPLICATIONS
+# --- REMOVE APPLICATIONS SECTION ---
 
-# Install elementary Tweaks
+
+
+# --- install THIRD-PARTY PACKAGE MANAGERS ---
+sudo apt install snapd
+
+# --- PACKAGE INSTALLATIN SECTION ---
+
+# install "elementary Tweaks"
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:pilip.scott/elementary0tweaks
 sudo apt update -y
 sudo apt install elementary-tweaks
 
 #
+sudo apt install neofetch
+
+#
 sudo apt install firefox
 
 sudo apt install chromium-browser
 
-sudo apt install neofetch
-
 sudo apt install youtube-dl
 
+# install Microsoft VS Code
+sudo snap install code --classic
