a48b6ea28c5b000930407d685a3546c500a6dfd0
58851 <34036509+58851@users.noreply.github.com>
2020-03-27 21:00:34 -0400

    Update elementary-os.sh

--- elementary-os.sh
+++ elementary-os.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+# --- the Standard Operating Procedure
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
 
-
-
-# --- install THIRD-PARTY PACKAGE MANAGERS ---
+# --- package managers
+sudo apt install gdebi-core
+sudo apt install pip
 sudo apt install snapd
-
-# --- PACKAGE INSTALLATIN SECTION ---
 
 # install "elementary Tweaks"
 sudo apt install software-properties-common
@@ -14,25 +16,44 @@ sudo add-apt-repository ppa:pilip.scott/elementary0tweaks
 sudo apt update -y
 sudo apt install elementary-tweaks
 
-#
+# --- IDEs
+sudo snap install atom
+# V.S. Code by Microsoft
+sudo snap install code --classic
+
+# --- MULTIMEDIA
+sudo apt install kdenlive
+sudo apt install krita
+sudo apt install inkscape
+
+# --- TeamViewer
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo gdebi install teamviewer_amd64.deb
+
+# --- UTILITIES
+# -- system information
 sudo apt install neofetch
-
-#
-sudo apt install firefox
-
-sudo apt install chromium-browser
-
+# -- terminal audio player
+sudo snap install tizonia
+# -- video downloader
 sudo apt install youtube-dl
 sudo youtube-dl -U
 
-# install Microsoft VS Code
-sudo snap install code --classic
+# --- VeraCrypt
+wget https://launchpad.net/veracrypt/trunk/1.24-update4/+download/veracrypt-console-1.24-Update4-Ubuntu-18.04-amd64.deb
+sudo gdebi install veracrypt-console-1.24-Update4-Ubuntu-18.04-amd64.deb
 
-# --- REMOVE APPLICATIONS SECTION ---
-sudo apt remove epiphany-browser
+# --- WEB BROWSERS
+sudo apt install chromium-browser
+sudo apt install firefox
+
+# --- remove media player
 sudo apt remove mpv
-# sudo apt remove [name of default email client]
 
-sudo apt update -y
-sudo apt upgrade -y
+# --- remove the default applications
+sudo apt remove epiphany-browser
+sudo apt remove pantheon-camera
+sudo apt remove pantheon-mail
+
+# --- do some housekeeping
 sudo apt autoremove -y
